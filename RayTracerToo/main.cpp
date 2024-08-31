39ef2d098f4c111448c9a2f79f7d80cabbed32b4
Deirdre Saoirse Moen <deirdre@deirdre.net>
2024-08-31 15:49:07 -0700

    Initial Commit

---
+++ RayTracerToo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RayTracerToo
+//
+//  Created by Deirdre Saoirse Moen on 8/31/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
